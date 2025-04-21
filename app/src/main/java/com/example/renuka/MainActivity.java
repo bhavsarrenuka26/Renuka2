c931e55a13450138acc4e2fdd0571a3dde6807c3
bhavsarrenuka26 <bhavsarrenuka12@gmail.com>
2025-04-21 18:52:25 +0530

    Initial commit

--- app/src/main/java/com/example/renuka/MainActivity.java
+++ app/src/main/java/com/example/renuka/MainActivity.java
@@ -2,14 +2,18 @@ package com.example.renuka;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
-        CardView cardProfile, cardRealtime, cardWeekly, cardMonthly, cardSuggestions;
+        CardView cardProfile, cardRealtime, cardWeekly, cardMonthly, cardSuggestions,cardLogout;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -21,11 +25,17 @@ public class MainActivity extends AppCompatActivity {
             cardWeekly = findViewById(R.id.card_weekly);
             cardMonthly = findViewById(R.id.card_monthly);
             cardSuggestions = findViewById(R.id.card_suggestions);
+            cardLogout=findViewById(R.id.card_logout);
 
             cardProfile.setOnClickListener(view -> startActivity(new Intent(this, ProfilePage.class)));
             cardRealtime.setOnClickListener(view -> startActivity(new Intent(this,GraphActivity.class)));
             cardWeekly.setOnClickListener(view -> startActivity(new Intent(this, WeeklyAnalysis.class)));
             cardMonthly.setOnClickListener(view -> startActivity(new Intent(this, MonthlyAnalysis.class)));
             cardSuggestions.setOnClickListener(view -> startActivity(new Intent(this, SuggestionActivity.class)));
+            cardLogout.setOnClickListener(v -> {
+                FirebaseAuth.getInstance().signOut();
+                startActivity(new Intent(getApplicationContext(), LoginPage.class));
+                finish();
+            });
         }
     }
